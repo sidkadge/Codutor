2eca5b0615c990a6cca3230882e5dc51a83a5ca3
sidkadge <siddheshkadgemitech@gmail.com>
2024-09-26 17:10:46 +0530

    setup done

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,3 +9,4 @@ $routes->get('/', 'Home::index');
 $routes->get('index', 'Home::index');
 $routes->get('header', 'Home::header');
 $routes->get('footer', 'Home::footer');
+$routes->get('AdminDashboard', 'AdminController::AdminDashboard');
